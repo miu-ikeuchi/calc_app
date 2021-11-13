f89b711de250923ea12eab0b6d2cd968a712f888
miu-ikeuchi <miu.ikeuchi7@gmail.com>
2021-11-13 11:56:12 +0900

    コントローラー・ビュー追加

---
+++ app/Http/Controllers/CalcsController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CalcsController extends Controller
+{
+    public function result($num1, $role, $num2)
+    {
+        
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/calcs/{num1}/{rule}/{num2}', [App\Http\Controllers\Calcs::class, 'result']);
